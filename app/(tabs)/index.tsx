ed6eebc2c88e324bbd30fac9a8aa50481357cf5b
rsayyed591 <rehansayyed591@gmail.com>
2025-04-26 17:20:06 +0530

    🌐 Chore: Updated main index.tsx for better routing and flow

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,10 +6,10 @@ import { StatusBar } from 'expo-status-bar';
 import { ThemeProvider } from './components/theme-provider';
 import HomeScreen from './screens/HomeScreen';
 import TranslateScreen from './screens/TranslateScreen';
-// import GalleryScreen from './screens/GalleryScreen';
+import GalleryScreen from './screens/GalleryScreen';
 import AboutScreen from './screens/AboutScreen';
-// import ModelsScreen from './screens/ModelsScreen';
-// import ModelDetailScreen from './screens/ModelDetailScreen';
+import ModelsScreen from './screens/ModelsScreen';
+import ModelDetailScreen from './screens/ModelDetailScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -26,10 +26,10 @@ export default function App() {
           >
             <Stack.Screen name="Home" component={HomeScreen} />
             <Stack.Screen name="About" component={AboutScreen} />
-             <Stack.Screen name="Translate" component={TranslateScreen} />
-            {/* <Stack.Screen name="Gallery" component={GalleryScreen} />
+            <Stack.Screen name="Translate" component={TranslateScreen} />
+            <Stack.Screen name="Gallery" component={GalleryScreen} />
             <Stack.Screen name="Models" component={ModelsScreen} />
-            <Stack.Screen name="ModelDetail" component={ModelDetailScreen} /> */}
+            <Stack.Screen name="ModelDetail" component={ModelDetailScreen} />
           </Stack.Navigator>
       </ThemeProvider>
     </SafeAreaProvider>
